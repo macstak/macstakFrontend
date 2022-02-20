f4f6b10be0993241e91195de3500610c5258bdc9
Manish Sahu <manissh7352@gmail.com>
2022-02-20 13:39:13 +0530

    error page ready

--- src/app/pages/error/page-not-found/page-not-found.component.ts
+++ src/app/pages/error/page-not-found/page-not-found.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+// @ts-ignore
+import Parallax from 'parallax-js';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
 })
 export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
+  constructor(public router: Router) {}
 
   ngOnInit(): void {
+    var scene = document.getElementById('scene');
+    var parallax = new Parallax(scene);
   }
 
+  gotToHomePage() {
+    this.router.navigateByUrl('');
+  }
 }
