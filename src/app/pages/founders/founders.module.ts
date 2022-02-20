0c300c6a87f8be818b084fc6c6f39a8abde6d999
Manish Sahu <manissh7352@gmail.com>
2022-02-20 19:40:07 +0530

    page not found completely fixed

--- src/app/pages/founders/founders.module.ts
+++ src/app/pages/founders/founders.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FoundersRoutingModule } from './founders-routing.module';
 import { FounderPageComponent } from './founder-page/founder-page.component';
-
+import { Shared1Module } from 'src/app/shared1/shared1.module';
 
 @NgModule({
-  declarations: [
-    FounderPageComponent
-  ],
-  imports: [
-    CommonModule,
-    FoundersRoutingModule
-  ]
+  declarations: [FounderPageComponent],
+  imports: [CommonModule, FoundersRoutingModule, Shared1Module],
 })
-export class FoundersModule { }
+export class FoundersModule {}
--- src/app/pages/home/homepage.module.ts
+++ src/app/pages/home/homepage.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
-
+import { Shared1Module } from 'src/app/shared1/shared1.module';
 
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule,
-    HomepageRoutingModule
-  ]
+  declarations: [HomePageComponent],
+  imports: [CommonModule, HomepageRoutingModule, Shared1Module],
 })
-export class HomepageModule { }
+export class HomepageModule {}
