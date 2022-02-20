e9f24ce0aab0a207529288ec758c3771c23efe18
Manish Sahu <manissh7352@gmail.com>
2022-02-20 20:54:27 +0530

    design 1 ready

--- src/app/pages/founders/founder-page/founder-page.component.ts
+++ src/app/pages/founders/founder-page/founder-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-founder-page',
   templateUrl: './founder-page.component.html',
-  styleUrls: ['./founder-page.component.scss']
+  styleUrls: ['./founder-page.component.scss'],
 })
 export class FounderPageComponent implements OnInit {
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/founders/founders.module.ts
+++ src/app/pages/founders/founders.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FoundersRoutingModule } from './founders-routing.module';
 import { FounderPageComponent } from './founder-page/founder-page.component';
 import { Shared1Module } from 'src/app/shared1/shared1.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [FounderPageComponent],
-  imports: [CommonModule, FoundersRoutingModule, Shared1Module],
+  imports: [
+    CommonModule,
+    FoundersRoutingModule,
+    FontAwesomeModule,
+    Shared1Module,
+  ],
 })
 export class FoundersModule {}
