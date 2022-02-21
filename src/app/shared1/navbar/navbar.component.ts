e5e96c19f7de7d81639988595c0af2272eb728df
Manish Sahu <manissh7352@gmail.com>
2022-02-21 12:14:11 +0530

    added mobile nav bar

--- src/app/shared1/navbar/navbar.component.ts
+++ src/app/shared1/navbar/navbar.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  public isMobileShow: any;
+  public isDesktopShow: any;
+  public innerWidth: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    // console.log(this.innerWidth);
+    // console.log(window.outerWidth);
+
+    if (this.innerWidth < 992 || window.outerWidth < 992) {
+      this.isMobileShow = true;
+      this.isDesktopShow = false;
+    } else {
+      // console.log('hi');
+
+      this.isMobileShow = false;
+      this.isDesktopShow = true;
+    }
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    // console.log(this.innerWidth);
+    // console.log(window.outerWidth);
+    // console.log(this.innerWidth);
+    if (this.innerWidth < 992 || window.outerWidth < 992) {
+      this.isMobileShow = true;
+      this.isDesktopShow = false;
+    } else {
+      console.log('hi');
+
+      this.isMobileShow = false;
+      this.isDesktopShow = true;
+    }
+  }
 }
--- src/app/shared1/shared1.module.ts
+++ src/app/shared1/shared1.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { Shared1RoutingModule } from './shared1-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MobileNavbarComponent } from './mobile-navbar/mobile-navbar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent, MobileNavbarComponent],
   imports: [CommonModule, Shared1RoutingModule],
   exports: [NavbarComponent, FooterComponent],
 })
