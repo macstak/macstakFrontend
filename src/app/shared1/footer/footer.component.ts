19d5a38a2e434dce1c1bd06986d2bdedb90dd9b3
Manish Sahu <manissh7352@gmail.com>
2022-02-21 22:38:42 +0530

    footer completed

--- src/app/shared1/footer/footer.component.ts
+++ src/app/shared1/footer/footer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared1/shared1.module.ts
+++ src/app/shared1/shared1.module.ts
@@ -5,10 +5,11 @@ import { Shared1RoutingModule } from './shared1-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MobileNavbarComponent } from './mobile-navbar/mobile-navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent, MobileNavbarComponent],
-  imports: [CommonModule, Shared1RoutingModule],
+  imports: [CommonModule, FontAwesomeModule, Shared1RoutingModule],
   exports: [NavbarComponent, FooterComponent],
 })
 export class Shared1Module {}
