6b35cdc0c5dd717227e90e8b91d0b822a8de9551
Manish Sahu <manissh7352@gmail.com>
2022-03-15 20:17:58 +0530

    made it ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent, CertificatePageComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
--- src/app/pages/error/page-not-found/page-not-found.component.ts
+++ src/app/pages/error/page-not-found/page-not-found.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 // @ts-ignore
 import Parallax from 'parallax-js';
 
+// @ts-ignore
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent implements OnInit {
+// @ts-ignore
+export class PageNotFoundComponent implements AfterContentInit {
   constructor(public router: Router) {}
 
-  ngOnInit(): void {
-    var scene = document.getElementById('scene');
-    var parallax = new Parallax(scene);
+  // @ts-ignore
+  ngAfterContentInit(): void {
+    const scene = document.getElementById('scene');
+    const parallax = new Parallax(scene, {
+      relativeInput: true,
+    });
   }
 
+  // @ts-ignore
   gotToHomePage() {
     this.router.navigateByUrl('');
   }
